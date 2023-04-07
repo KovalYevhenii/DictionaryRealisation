4196637732a7983e6855a0addbd5fe3bd3c9a7ac
KovalYevhenii <kovalyevhenii@gmail.com>
2023-04-07 18:31:18 +0200

    test

--- DictioanaryOpenAdressingLinearProbing/OtusDictionary.cs
+++ DictioanaryOpenAdressingLinearProbing/OtusDictionary.cs
@@ -1,91 +1,117 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+using System.Xml;
 
 namespace DictioanaryOpenAdressingLinearProbing
 {
     public class OtusDictionary
     {
         private int _capacity;
-        private int _count;
         private int[] _keys;
         private string[] _values;
-        public OtusDictionary(int initialCapacity = 2)
+        public OtusDictionary(int initialCapacity)
         {
             _keys = new int[initialCapacity];
             _values = new string[initialCapacity];
             _capacity = initialCapacity;
         }
+
         public void Add(int key, string value)
         {
-            int index = key % _capacity;
+            try
+            {
+                if (key == 0)
+                {
+                    Console.WriteLine("Key cannot be equal to 0.");
+                    return;
+                }
+                int index = FindEmptySlot(key);
+
+                if (_keys[index] == key)
+                {
+                    throw new Exception();
+                }
+
+                _values[index] = value ?? throw new ArgumentNullException(nameof(value));
+                _keys[index] = key;
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine($"null value not permitted.{ex.Message}");   
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine($"Key {key} already exists. Value will not be overridden. {ex.Message}");
+            }
+        }
+
+        private int FindEmptySlot(int key)
+        {
+            int index = Math.Abs(key) % _capacity;
+       
             int originalIndex = index;
 
             while (_keys[index] != 0 && _keys[index] != key)
             {
                 index = (index + 1) % _capacity;
-                if (index == originalIndex)
+
+                if (index != originalIndex)
                     continue;
-                //Table is full and key is not found,so double the capacity
-                int newCapacity = _capacity * 2;
-                int[] newKeys = new int[newCapacity];
-                string[] newValues = new string[newCapacity];
 
-                //rehash all existing keys and values
-                for (int i = 0; i < _capacity; i++)
-                {
-                    if (_keys[i] != 0)
-                    {
-                        int newIndex = _keys[i] % newCapacity;
-                        while (newKeys[newIndex] != 0)
-                        {
-                            newIndex = (newIndex + 1) % newCapacity;
-                        }
-                        newKeys[newIndex] = _keys[i];
-                        newValues[newIndex] = _values[i];
-                    }
-                }
-                _keys = newKeys;
-                _values = newValues;
-                _capacity = newCapacity;
-                //recalculate the index for the new key
-                index = key % _capacity;
+                ResizeAndRehash();
+                index = Math.Abs(key) % _capacity;
                 originalIndex = index;
-
             }
-            _keys[index] = key;
-            _values[index] = value;
-            _count++;
+
+            return index;
         }
+
+        private void ResizeAndRehash()
+        {
+            int newCapacity = _capacity * 2;
+            int[] newKeys = new int[newCapacity];
+            string[] newValues = new string[newCapacity];
+
+            for (int i = 0; i < _capacity; i++)
+            {
+                    int newIndex = FindEmptySlot(_keys[i]);
+                    newKeys[newIndex] = _keys[i];
+                    newValues[newIndex] = _values[i];
+            }
+
+            _keys = newKeys;
+            _values = newValues;
+            _capacity = newCapacity;
+        }
+
         public string Get(int key)
         {
-            int index = key % _capacity;
-            int originalIndex = index;
 
-            while (_keys[index] != 0 && _keys[index] != key)
-            {
-                index = (index + 1) % _capacity;
+            int index = FindKeyIndex(key);
 
-                if (index == originalIndex)
-                {
-                    //key is not found
-                    throw new KeyNotFoundException();
-                }
-            }
             if (_keys[index] == 0)
             {
-                throw new KeyNotFoundException();
+                Console.WriteLine("Key does not exist ");
             }
+
             return _values[index];
         }
-        //Indexer
+
+        public int FindKeyIndex(int key)
+        {
+            int index = Math.Abs(key) % _capacity;
+
+            while (_keys[index] != 0 && _keys[index] != key && index != 0)
+            {
+                index = (index + 1) % _capacity;
+            }
+
+            return index;
+        }
+
         public string this[int key]
         {
             get => Get(key);
             set => Add(key, value);
         }
-
     }
 }
--- DictioanaryOpenAdressingLinearProbing/Program.cs
+++ DictioanaryOpenAdressingLinearProbing/Program.cs
@@ -4,16 +4,56 @@
     {
         static void Main(string[] args)
         {
-            OtusDictionary dictionary = new();
-            dictionary.Add(-2, "sd");
-            dictionary.Add(-12, "0reo");
-            dictionary.Add(3, "dwd");
-            dictionary.Add(4, "bmw");
-            dictionary.Add(5, "bmw");
+            var dictionary = new OtusDictionary(32);
 
-           // dictionary.Get(-1);
+            dictionary.Add(2, null);
+            dictionary.Add(0, "0");
+            dictionary.Add(10, "Value10"); 
+            dictionary.Add(32, "Value32");
+            dictionary.Add(25, "Value25");
+            dictionary.Add(7, "Value7");
+            dictionary.Add(3, "Value3");
+            dictionary.Add(12, "Value12");
+            dictionary.Add(20, "Value20");
+            dictionary.Add(18, "Value18");
+            dictionary.Add(29, "Value29");
+            dictionary.Add(5, "Value5");
+            dictionary.Add(21, "Value21");
+            dictionary.Add(9, "Value9");
+            dictionary.Add(27, "Value27");
+            dictionary.Add(8, "Value8");
+            dictionary.Add(23, "Value23");
+            dictionary.Add(14, "Value14");
+            dictionary.Add(17, "Value17");
+            dictionary.Add(22, "Value22");
+            dictionary.Add(6, "Value6");
+            dictionary.Add(31, "Value31");
+            dictionary.Add(24, "Value24");
+            dictionary.Add(11, "Value11");
+            dictionary.Add(30, "Value30");
+            dictionary.Add(16, "Value16");
+            dictionary.Add(4, "Value4");
+            dictionary.Add(26, "Value26");
+            dictionary.Add(1, "Value1");
+            dictionary.Add(13, "Value13");
+            dictionary.Add(19, "Value19");
+            dictionary.Add(28, "Value28");
+            dictionary.Add(2, "Value2");
+            dictionary.Add(234, "Value234");
+            dictionary.Add(64, "Value64");
+            dictionary.Add(64, "Value64");
+            dictionary.Add(65, "Value65");
+     
+            Console.WriteLine(dictionary[66]);
+            Console.WriteLine(dictionary[1]);
+            Console.WriteLine(dictionary[-1]);
             
-
+         
         }
     }
 }
+
+
+
+
+
