a89d21c0f66511e83f232d0955458e2ea46fc75f
KovalYevhenii <kovalyevhenii@gmail.com>
2023-04-07 18:31:18 +0200

    test

--- DictioanaryOpenAdressingLinearProbing/Program.cs
+++ DictioanaryOpenAdressingLinearProbing/Program.cs
@@ -4,15 +4,11 @@
     {
         static void Main(string[] args)
         {
-            OtusDictionary dictionary = new();
-            dictionary.Add(-2, "sd");
-            dictionary.Add(-12, "0reo");
-            dictionary.Add(3, "dwd");
-            dictionary.Add(4, "bmw");
-            dictionary.Add(5, "bmw");
 
-           // dictionary.Get(-1);
-            
+            OtusDictionary dictionary = new();
+            dictionary.Add(1, "hello");
+            dictionary.Add(-1, "world");
+            dictionary.Get(1);
 
         }
     }
